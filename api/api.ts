a0b8e9df251e83a58645e18ea5f2bb809b895d2a
Nguyen Tuan Kiet <ntk28042002@gmail.com>
2024-10-09 14:19:53 +0700

    refactor: remove function not use

--- api/api.ts
+++ api/api.ts
@@ -1,11 +1,1 @@
-import axios from "axios";
-
 export const BASE_URL = "https://66f7c380b5d85f31a3438d56.mockapi.io/api";
-
-const apiManager = axios.create({
-  baseURL: BASE_URL,
-  responseType: "json",
-  withCredentials: true,
-});
-
-export default apiManager;
